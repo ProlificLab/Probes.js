74bb4e9a4bd3e21c9f1abb32a97dc263f5b10367
Christophe <c.harrer@prolificlab.com>
2012-04-25 15:00:17 +0200

    Nex Cpu, Memory and Process functions

--- Readme.md
+++ Readme.md
@@ -1,1 +1,66 @@
-# Usage
+Probes.js
+=======
+
+* A Node.js module to get server load statistics
+
+Probes.js provides a simple toolkit to measure the load of a server and to get basic information for a running process.
+It has been tested to work on Mac Os X and Linux.
+
+Basic Example
+-------------
+**Installation**
+npm install Probes.js
+
+**How to Use**
+
+```javascript
+var probes = require('Probes.js);
+
+//Print the number of hard drives connected to the server
+probes.hardDriveProbe(function(err,results){
+    if(!err)console.log(results.length);
+});
+```
+
+Function Documentation
+----------------------
+
+**hardDriveProbe**
+
+```javascript
+probes.hardDriveProbe(function(err,results){});
+
+results = [{fileSystem : name, totalSpace : Ko, usedSpace : Ko, availableSpace : Ko, percentUsed : percent, mountPoint : path}];
+```
+
+**folderProbe**
+
+```javascript
+probes.folderProbe(path,function(err,results){});
+
+results = folderSizeKo;
+```
+
+**cpuProbe**
+
+```javascript
+probes.cpuProbe(function(err,results){});
+
+results = { spot : percentRigthNow, one : averageLastMinute, five : averageLastFiveMinutes,fifteen : averageLastFifteenMinutes};
+```
+
+**memoryProbe**
+
+```javascript
+probes.memoryProbe(function(err,results){});
+
+results = {freeMemory : freeMemory, activeMemory : usedMemory, totalMemory : totalMemory};
+```
+
+**processListProbe**
+
+```javascript
+probes.processListProbe(function(err,results){});
+
+results = [{pid : processPid, name : commandName, time : elapsedTime, pcpu : percentOfCpuUsed, pmem : percentOfMemoryUsed}];
+```
--- module/probes.js
+++ module/probes.js
@@ -1,30 +1,11 @@
-var spawn, async, // Modules
+var spawn, async,os, // Modules
     getCmdOutput; //Functions
 
 spawn = require('child_process').spawn;
 async = require('async');
+os = require('os');
 
-getCmdOutput = function getCmdOutput(cmd, args, callback) {
-    "use strict";
-    var process, outputLines, err;
-    process = spawn(cmd, args);
-    outputLines = [];
-    err = null;
-
-    process.stdout.on("data", function onOut(buf) {
-        outputLines.push(buf.toString("ascii"));
-    });
-
-    process.on("error", function onError(error) {
-        err = error;
-    });
-
-    process.on("exit", function onExit() {
-        callback(err, outputLines.join("\n"));
-    });
-};
-
-exports.hardDriveProbe = function disksize(callback){
+exports.hardDriveProbe = function hardDriveProbe(callback){
     "use strict";
     var array, searchIndex, elements, results;
     results = [];
@@ -50,5 +31,89 @@ exports.hardDriveProbe = function disksize(callback){
             callback();
         });
         callback(null,results);
+    });
+};
+
+exports.folderProbe = function folderProbe(path,callback){
+    "use strict";
+    var array, searchIndex, elements, results;
+    results = [];
+    getCmdOutput("du",["-s","-k",path],function(err,output){
+        callback(null,parseInt(output.split(/\s+/)[0],10));
+    });
+};
+
+exports.cpuProbe = function cpuProbe(callback){
+    "use strict";
+    var osLoadAvg, osCoreNumber, arrayDoc, arrayLine, spotProc, searchIndex;
+    osLoadAvg = os.loadavg();
+    osCoreNumber = os.cpus().length;
+    getCmdOutput("ps",["ax", "-eo pcpu"],function(err,output){
+        arrayDoc = output.split(/\n/);
+        arrayDoc.splice(0,1);
+        searchIndex = arrayDoc.indexOf(""); // Find the index
+        if(searchIndex !== -1) {
+            arrayDoc.splice(searchIndex, 1);// Remove it if really found!
+        }
+        spotProc = 0;
+        async.forEach(arrayDoc,function(line,callback){
+            if(!isNaN(line)){
+                spotProc = spotProc + parseFloat(line);
+            }
+            callback();
+        });
+        callback(null,{spot:spotProc,one:osLoadAvg[0]/osCoreNumber*100,five:osLoadAvg[1]/osCoreNumber*100,fifteen:osLoadAvg[2]/osCoreNumber*100});
+    });
+};
+
+exports.processListProbe = function processListProbe(callback){
+    "use strict";
+    var arrayDoc, results, searchIndex, elements;
+    results=[];
+    getCmdOutput("ps",["ax", "-eo etime,pcpu,pmem,pid,args"],function(err,output){
+        arrayDoc = output.split(/\n/);
+        arrayDoc.splice(0,1);
+        searchIndex = arrayDoc.indexOf(""); // Find the index
+        if(searchIndex !== -1) {
+            arrayDoc.splice(searchIndex, 1);// Remove it if really found!
+        }
+        async.forEach(arrayDoc,function(line,callback){
+            elements = line.split(/\s+/);
+            searchIndex = elements.indexOf(""); // Find the index
+            if(searchIndex !== -1) {
+                elements.splice(searchIndex, 1);// Remove it if really found!
+            }
+            results.push({time:elements.splice(0,1).join(),pcpu:parseFloat(elements.splice(0,1).join()),pmem:parseFloat(elements.splice(0,1).join()),pid:parseInt(elements.splice(0,1),10),name:elements.join(" ")});
+            callback();
+        });
+        callback(null,results);
+    });
+};
+
+exports.memoryProbe = function memoryProbe(callback){
+    "use strict";
+    var osFreeMemory, osTotalMemory;
+    osFreeMemory = os.freemem();
+    osTotalMemory = os.totalmem();
+    callback(null,{freeMemory: osFreeMemory,activeMemory: osTotalMemory-osFreeMemory,totalMemory: osTotalMemory});
+};
+
+getCmdOutput = function getCmdOutput(cmd, args, callback) {
+    "use strict";
+    var process, outputLines, err;
+    process = spawn(cmd, args);
+    outputLines = [];
+    err = null;
+
+    process.stdout.on("data", function onOut(buf) {
+        outputLines.push(buf.toString("ascii"));
+    });
+
+    process.on("error", function onError(error) {
+        err = error;
+    });
+
+    process.on("exit", function onExit() {
+        callback(err, outputLines.join("\n"));
     });
 };
